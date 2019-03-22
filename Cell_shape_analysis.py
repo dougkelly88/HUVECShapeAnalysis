30ff6c42d3b76b55a65c1f63245dce05f23c5201
douglas kelly <douglas.kelly@riken.jp>
2019-03-22 14:33:04 +0900

    Bump version string

--- Cell_shape_analysis.py
+++ Cell_shape_analysis.py
@@ -23,7 +23,7 @@ class Parameters(object):
 	# constants
 	_persist_parameters_filename = "IJ_cell_shape_analysis_params.json"
 	_persist_parameters_folder = "IJ_cell_shape_analysis";
-	_version_string = "0.0.3";
+	_version_string = "0.0.4";
 
 	_analysis_modes = ["GFP intensity", 
 						"GFP intensity + manual correction", 
